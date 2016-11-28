5cd0186bda7d730128408b9675efc36700bc627c
Martin Schmid <m.schmid@equitania.com>
2016-11-28 21:27:09 +0100

    [ADD] Neues Backend Theme für Odoo 10 von MyOdoo.de

---
+++ addons/eq_myodoo_backend_theme/RELEASE_NOTES.md
@@ -0,0 +1,35 @@
+## Modul eq_myodoo_backend_theme
+
+
+### 28.11.2016
+### Version 1.0.5
+#### FIX
+- Manipulation eines Farbwertes "transparent" war nicht möglich.
+
+### 25.11.2016
+### Version 1.0.4
+#### CHG
+- unnötige Verlinkung auf eine Javascript gelöscht
+
+### 24.11.2016
+### Version 1.0.3
+#### CHG
+- Weitere Code Änderungen
+
+
+### Version 1.0.2
+#### CHG
+- kleine Code Änderung
+
+
+### Version 1.0.1
+#### IMP
+- Farbanpassungen
+- Korrektur der Schrift für Dropbdown Boxes
+- Front-End Styles hinzugefügt
+- Tooltip angepasst
+
+### 23.11.2016
+### Version 1.0.0
+#### ADD
+- Initial Check-in
---
+++ addons/eq_myodoo_backend_theme/__manifest__.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo Addon, Open Source Management Solution
+#    Copyright (C) 2014-now Equitania Software GmbH(<http://www.equitania.de>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    # Theme information
+    'name' : 'MyOdoo Backend Theme v10',
+    'category' : 'Website',
+    'version' : '1.0.6',
+    'license': 'AGPL-3',
+    'summary': 'Backend, Theme',
+    'description': """
+
+    """,
+    'images': ['static/description/icon.png'],
+
+    # Dependencies
+    'depends': [
+        'web',
+    ],
+    'external_dependencies': {},
+
+    # Views
+    'data': [
+       'views/backend.xml',
+       'views/frontend.xml',
+    ],
+    'qweb': [
+        'static/src/xml/web.xml',
+    ],
+
+    # Author
+    'author': 'Equitania Software GmbH',
+    'website': 'https://www.myodoo.de',
+
+    # Technical
+    'installable': True,
+    'auto_install': True,
+    'application': True,
+
+}
