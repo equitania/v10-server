a2a951dd84796b4b9ece441426485ff417a7dc95
Julia Eberle <j.eberle@equitania.de>
2017-07-17 14:42:52 +0200

    [CHG/ADD]- Icons für UTM und Website Links hinzugefügt, Menü Item Verlinkung für UTM bearbeitet, Menü Item Verlinkung für Link Tracker wieder entfernt

--- addons/link_tracker/__manifest__.py
+++ addons/link_tracker/__manifest__.py
@@ -6,7 +6,7 @@ Create short and trackable URLs.
 =====================================================
 
         """,
-    'version': '1.0.1',
+    'version': '1.0.2',
     'depends': ['utm'],
     'data': [
         'views/link_tracker.xml',
